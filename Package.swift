529a9a683fd1c7dc66f62c2e08b171c1c0f6bbe1
Ade Dwi Prayitno <adedwip1808@gmail.com>
2026-01-17 00:23:26 +0700

    Bump version to 1.0.2

--- Package.swift
+++ Package.swift
@@ -10,9 +10,9 @@ let package = Package(
         .library(name: "Home", targets: ["Home"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/adedwi1808/game-catalogue-Core-package.git", from: "1.0.1"),
-        .package(url: "https://github.com/adedwi1808/game-catalogue-Common-package.git", from: "1.0.1"),
-        .package(url: "https://github.com/adedwi1808/game-catalogue-Components-package.git", from: "1.0.1"),
+        .package(url: "https://github.com/adedwi1808/game-catalogue-Core-package.git", from: "1.0.2"),
+        .package(url: "https://github.com/adedwi1808/game-catalogue-Common-package.git", from: "1.0.2"),
+        .package(url: "https://github.com/adedwi1808/game-catalogue-Components-package.git", from: "1.0.2"),
         
         .package(url: "https://github.com/Juanpe/SkeletonView.git", from: "1.31.0")
     ],
